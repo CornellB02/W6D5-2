941d7f4b43e784db77d3e2c6e65f59bcec12626c
Cornell Bethea <betheacornellb@gmail.com>
2022-12-16 17:14:24 -0500

    birthday

--- ninety-nine-cats/app/models/Cat.rb
+++ ninety-nine-cats/app/models/Cat.rb
@@ -3,14 +3,18 @@ require "date"
 class Cat < ApplicationRecord
     CAT_COLOR = ["red", "green", "blue", "yellow"]
 
-    validates :birth_date, presence: true 
+    validates :birth_date_cannot_be_future, presence: true 
+
     validates :color, inclusion: { in: CAT_COLOR }
     validates :sex, inclusion: { in: ["M", "F"] }
+    validates :name, presence: true
 
     def birth_date_cannot_be_future
-        if birth_date.present? && birth_date < Date.today
-            error.add(:birth_date, "Can't be added")
+        if birth_date > Date.today
+            errors.add(:birth_date, "Can't be added")
         end 
     end
+
+    
 
 end
