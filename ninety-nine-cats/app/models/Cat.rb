f7fb4904c1dd2f4f98647a4e7912e8758f1dc4f2
Lynsie Aragon <lynsie.aragon@gmail.com>
2022-12-16 16:34:36 -0500

    birth_date_cannot_be_future complete

--- ninety-nine-cats/app/models/Cat.rb
+++ ninety-nine-cats/app/models/Cat.rb
@@ -1,5 +1,16 @@
-class Cat < ApplicationRecord
+require "date"
 
-    validates :birth_date, presence: true, 
+class Cat < ApplicationRecord
+    CAT_COLOR = ["red", "green", "blue", "yellow"]
+
+    validates :birth_date, presence: true 
+    validates :color, inclusion: { in: CAT_COLOR }
+    validates :sex, inclusion: { in: ["M", "F"] }
+
+    def birth_date_cannot_be_future
+        if birth_date.present? && birth_date < Date.today
+            error.add(:birth_date, "Can't be added")
+        end 
+    end
 
 end
